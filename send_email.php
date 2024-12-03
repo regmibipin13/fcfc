08cb7862b416cf624cfec43a9c29237079f68846
Bipin Regmi <bipin.regmi.s21@gmail.com>
2024-12-03 22:00:05 +0545

    Done

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Admin email
+    $to = "bipin.regmi.s21@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "You have received a new message from the contact form.\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Thank you for your message! We will get back to you shortly.'); window.location.href='contact.html';</script>";
+    } else {
+        echo "<script>alert('Sorry, something went wrong. Please try again later.'); window.location.href='contact.html';</script>";
+    }
+} else {
+    header("Location: contact.html");
+    exit;
+}
